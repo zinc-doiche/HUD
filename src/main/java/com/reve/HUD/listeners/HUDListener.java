53bd82901419325a40b6c28e9ae86b10d346186a
조성진 <lmsj122@gachon.ac.kr>
2022-08-29 18:17:41 +0900

    add stamina display system (frontend)

--- src/main/java/com/reve/HUD/listeners/HUDListener.java
+++ src/main/java/com/reve/HUD/listeners/HUDListener.java
@@ -34,8 +34,11 @@ public class HUDListener implements Listener{
 
         new ThirstConfig(plugin, player).setThirstConfig();
         //player.sendMessage("Thirst: "+Thirst.getThirst(id));
+
         new ThirstTask(plugin, id).runTaskTimer(plugin, 5L, 5L);
+
         new TemperatureTask(plugin, id).runTaskTimer(plugin, 10L, 10L);
+
         new DisplayTask(plugin, id).runTaskTimer(plugin, 0L, 1L);
     }
     @EventHandler
